9176ef4cd78240d1a11281384cf375cc3f2b9283
Rhythmkamra <itsmerhythmk@gmail.com>
2024-02-03 19:12:53 +0530

    array operations

--- array/basic.cpp
+++ array/basic.cpp
@@ -9,5 +9,24 @@ int main(){
     cout<<endl;
     cout<<&ch<<endl; 
     // {both of  these give  the base address }
-    cout<<ch;
+    cout<<ch<<endl;
+    // indexing of array
+
+
+    // printing an array
+    int arr[5]={1,3,4,5,6};
+    for(int i=0;i<5;i++){
+        cout<<"value of the"<<" "<<i+1<<" "<<"element is"<<" "<<arr[i];
+        cout<<endl;
+    }
+    int crr[5];
+    for(int i=0;i<5;i++){
+        cout<<"enter element"<<endl;
+        cin>>crr[i];
+        
+    }
+    for(int i=0;i<5;i++){
+        cout<<"value of the"<<" "<<i+1<<" "<<"element is"<<" "<<crr[i];
+        cout<<endl;}
+    return 0;
 }
