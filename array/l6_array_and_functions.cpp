3c7bafc7b76b3562f2f0fde227a3e29be4f4f78c
Rhythmkamra <itsmerhythmk@gmail.com>
2024-02-04 08:53:05 +0530

    boolean-function-note

--- array/l6_array_and_functions.cpp
+++ array/l6_array_and_functions.cpp
@@ -3,23 +3,30 @@ using namespace std;
 void solve(int arr[],int n){
     cout<<"function executed";
 }
-void linear_search(int arr[],int n,int target)
-{
-   for(int i=0;i<n;i++){
-     if (arr[i]==target){
-        cout<<true;
-        break;
+bool linear_search(int arr[], int n, int target) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == target) {
+            return true;  // Return true if the target is found
+        }
     }
-    else{
-        cout<<false;
-    }
-   }
+    return false;  // Return false if the target is not found after the loop
 }
+
 int main(){
     int crr[3]={8,7,6};
     int size = 3;
-    solve(crr,size);
+    // solve(crr,size);
     // always pass the size of the array with the function
-    linear_search(crr,3,7);
+    int output=linear_search(crr,3,7);
+    if (output=1){
+        cout<<"element found";
+    }
+    else{
+        cout<<"element not found";
+    }
+    
 
 }
+// note :-the  fucntion with return type of boolean dont show the output inme value 0,1 ho jati hai fucntion ki
+// aur fucntion ki value ko hum log variable mei store krwa dete hai aur fir use if else block se use krte hai
+// i.e explicit output 
