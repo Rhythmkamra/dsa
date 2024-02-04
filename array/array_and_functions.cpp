77d04bfc37a067767ec9b374642f068cb6334f96
Rhythmkamra <itsmerhythmk@gmail.com>
2024-02-04 08:27:29 +0530

    function_array

---
+++ array/array_and_functions.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+void solve(int arr[],int n){
+    cout<<"function executed";
+}
+int main(){
+    int crr[3]={8,7,6};
+    int size = 2;
+    solve(crr,size);
+}
